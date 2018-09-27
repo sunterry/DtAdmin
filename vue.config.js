0857a81b9068b01010656cf918d3ab74fd99446b
duantong <2664787866@qq.com>
2018-09-27 21:07:54 +0800

    axios

--- vue.config.js
+++ vue.config.js
@@ -26,7 +26,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/admin': {
-        target: 'http://192.168.1.133:927',
+        target: 'http://60.213.62.107:927',
         changeOrigin: true,
         pathRewrite: {
           '^/admin': '/admin',
